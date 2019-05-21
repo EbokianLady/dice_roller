b9862fdd3edb9a52b50ad1b995c09533d793ae35
Stephanie Keller <stephani.kell@gmail.com>
2019-05-21 18:33:18 -0400

    add d6 icons

---
+++ src/components/dice_images.js
@@ -0,0 +1,7 @@
+import icon1d6 from '../images/1d6.png';
+import icon2d6 from '../images/2d6.png';
+
+export const diceImages = {
+  '1d6': icon1d6,
+  '2d6': icon2d6,
+};
--- src/components/dice_roller.js
+++ src/components/dice_roller.js
@@ -1,18 +1,27 @@
 import React, { useState } from 'react';
 import './dice_roller.css';
+import { diceImages } from './dice_images';
+
 
 function DiceRoller() {
   const [diceCount, setDiceCount] = useState(2);
   const [diceValue, setDiceValue] = useState(6);
+  const [diceImage, setDiceImage] = useState('2d6');
   const [rollValue, setRollValue] = useState(0);
   const [rollSummary, setRollSummary] = useState('');
 
   const [explodes, setExplodes] = useState(false);
   const [dropDown, setDropDown] = useState(false);
 
-  const setDiceRoller = function(count, value) {
+  // const diceImages = {
+  //   '1d6': icon1d6,
+  //   '2d6': icon1d6,
+  // };
+
+  const setDiceRoller = function(count, value, image) {
     setDiceCount(count);
     setDiceValue(value);
+    setDiceImage(image);
   };
 
   const rollDice = function() {
@@ -26,6 +35,8 @@ function DiceRoller() {
       total += random;
     }
 
+    str += ' =';
+
     setRollSummary(str.slice(3));
     setRollValue(total);
   };
@@ -34,18 +45,13 @@ function DiceRoller() {
     <div>
       <button
         className='dice-option'
-        onClick={() => setDiceRoller(1, 4)}>
-          1d4
+        onClick={() => setDiceRoller(1, 6, '1d6')}>
+          <img className='dice-icon' src={diceImages['1d6']} />
       </button>
       <button
         className='dice-option'
-        onClick={() => setDiceRoller(1, 6)}>
-          1d6
-      </button>
-      <button
-        className='dice-option'
-        onClick={() => setDiceRoller(2, 6)}>
-          2d6
+        onClick={() => setDiceRoller(2, 6, '2d6')}>
+        <img className='dice-icon' src={diceImages['2d6']} />
       </button>
     </div>
   )
@@ -61,15 +67,16 @@ function DiceRoller() {
           ROLL
         </div>
         <div className='dice-selection'>
-          {diceCount}D{diceValue}
+          <img className='dice-icon' src={diceImages[diceImage]} />
         </div>
-        <button onClick={() => setExplodes(!explodes)}>
+        <div
+          className='dice-explodes'
+          onClick={() => setExplodes(!explodes)}>
           {bool}
-        </button>
+        </div>
         <div className='dice-summary'>
-          <h3>{rollSummary}</h3>
-          <h4>=</h4>
-          <h2>{rollValue}</h2>
+          <h3 className='dice-rolls'>{rollSummary}</h3>
+          <h2 className='dice-total'>{rollValue}</h2>
         </div>
       </div>
 
