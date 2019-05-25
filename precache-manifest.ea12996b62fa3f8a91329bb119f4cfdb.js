5aacb4b3c59a702ab54207239d2b568f6d744fe5
Stephanie Keller <stephani.kell@gmail.com>
2019-05-25 01:34:28 -0400

    Updates

---
+++ precache-manifest.ea12996b62fa3f8a91329bb119f4cfdb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "679c18fdd66c34cb7c478dcf955805fb",
+    "url": "/dice_roller/index.html"
+  },
+  {
+    "revision": "9ffa96b6be561dc6a75e",
+    "url": "/dice_roller/static/css/main.5c70bf74.chunk.css"
+  },
+  {
+    "revision": "24213d140fe4717ffb4f",
+    "url": "/dice_roller/static/js/2.b5a8bbb3.chunk.js"
+  },
+  {
+    "revision": "9ffa96b6be561dc6a75e",
+    "url": "/dice_roller/static/js/main.5e50869d.chunk.js"
+  },
+  {
+    "revision": "21b56bdec9fad1d6d663",
+    "url": "/dice_roller/static/js/runtime~main.80c3e5bc.js"
+  },
+  {
+    "revision": "1bac40b41b7c38ee71c027bbb551d75e",
+    "url": "/dice_roller/static/media/logo.1bac40b4.png"
+  }
+]);
